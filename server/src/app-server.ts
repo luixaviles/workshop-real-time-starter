f963dd3a74e75ba2de75807e3e3ea25835160e6c
luixaviles <luis.avilesz@gmail.com>
2018-10-18 02:24:51 -0300

    lab-01

--- server/src/app-server.ts
+++ server/src/app-server.ts
@@ -1,13 +1,19 @@
+import { createServer, Server } from 'http';
+import * as socketIo from 'socket.io';
 import * as express from 'express';
 
 export class AppServer {
     public static readonly PORT = 3000;
     private app: express.Express;
     private port: string | number;
+    private io: SocketIO.Server;
+    private server: Server;
 
     constructor() {
         this.port = process.env.PORT || AppServer.PORT;
         this.app = express();
+        this.server = createServer(this.app);
+        this.io = socketIo(this.server);
     }
 
     public listen() {
