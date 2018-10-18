8339ab1de9ca2ea6d2612b770f01458bcbb56de5
luixaviles <luis.avilesz@gmail.com>
2018-10-18 02:52:05 -0300

    lab-05

---
+++ client/src/app/shared/socket.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import * as socketIo from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+
+  constructor() {
+    this.socket = socketIo('http://localhost:3000');
+   }
+
+  public sendMessage(message: Message): void {
+    this.socket.emit('message', message);
+  }
+
+  public onMessage(): Observable<Message> {
+    return new Observable<Message>(observer => {
+      this.socket.on('message', (data: Message) => observer.next(data));
+    });
+  }
+}
