5585ca4501d5564f77c462203794a4ce6490e55c
luixaviles <luis.avilesz@gmail.com>
2018-10-18 02:35:16 -0300

    lab-03

--- server/src/app-server.ts
+++ server/src/app-server.ts
@@ -1,6 +1,8 @@
 import { createServer, Server } from 'http';
 import * as socketIo from 'socket.io';
 import * as express from 'express';
+import { Message } from './message';
+import { MessageGenerator } from './message-generator';
 
 export class AppServer {
     public static readonly PORT = 3000;
@@ -22,10 +24,22 @@ export class AppServer {
         });
 
         this.io.on('connect', (socket: SocketIO.Socket) => {
+            const generator = new MessageGenerator();
             console.log('Client connected');
+
+            socket.on('message', (m: Message) => {
+                console.log('Message received: ' + JSON.stringify(m));
+            });
+
+            let interval = setInterval(() => {
+                const message = generator.generateMessage();
+                console.log('sending message: ' + JSON.stringify(message));
+                this.io.emit('message', message);
+            }, 1000);
 
             socket.on('disconnect', () => {
                 console.log('Client disconnected');
+                clearInterval(interval);
             });
         });
     }
